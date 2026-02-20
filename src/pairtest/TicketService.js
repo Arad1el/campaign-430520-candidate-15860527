d24d7a204637ce0adc38eebacfc65afc11b28518
Aradiel <25931501+Arad1el@users.noreply.github.com>
2026-02-20 22:40:36 +0000

    Validation logic and accompanying tests

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -1,5 +1,6 @@
 import TicketTypeRequest from './lib/TicketTypeRequest.js';
 import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
+import TicketPaymentService from '../thirdparty/paymentgateway/TicketPaymentService.js';
 
 export default class TicketService {
   /**
@@ -8,21 +9,76 @@ export default class TicketService {
 
   purchaseTickets(accountId, ...ticketTypeRequests) {
     //account id must be an integer above 0
-    if (!Number.isInteger(accountId)) throw new InvalidPurchaseException("Account ID must be an integer");
-    if (accountId <= 0) throw new InvalidPurchaseException("Account ID must be greater than 0");
+    if (!Number.isInteger(accountId)) {
+      throw new InvalidPurchaseException("Account ID must be an integer");
+    }
+    if (accountId <= 0) {
+      throw new InvalidPurchaseException("Account ID must be greater than 0");
+    }
 
     //total number of tickets in the ticket requests must be less than or equal to 25
+    const ticketCount = this.#getTotalNumberOfTickets(ticketTypeRequests);
+    if (ticketCount <= 0 || ticketCount > 25) {
+      throw new InvalidPurchaseException("Total ticket count must be between 1 and 25");
+    }
 
+    const groupedRequests = this.#groupTicketRequestByType(ticketTypeRequests);
     //if any non-adult tickets, there must also be at least 1 adult ticket
+    if (groupedRequests.CHILD + groupedRequests.INFANT > 0 && groupedRequests.ADULT === 0) {
+      throw new InvalidPurchaseException("There must be at least 1 ADULT ticket if there are non-ADULT tickets");
+    }
 
     //number of infant seats must be less than or equal to number of adult tickets
+    if (groupedRequests.INFANT > groupedRequests.ADULT) {
+      throw new InvalidPurchaseException(`Since INFANTS will sit on ADULT laps,
+        there must be more ADULT (${groupedRequests.ADULT}) than INFANT (${groupedRequests.INFANT}) tickets`);
+    }
 
     //calculate correct payment amount
     //send payment request for that amount
+    const paymentService = new TicketPaymentService();
+    paymentService.makePayment(1, 1);
 
     //calculate correct number of seats
     //send seat reservation request for that amount
 
     // throws InvalidPurchaseException
+  }
+
+  #getTotalNumberOfTickets(ticketTypeRequests) {
+    let numberOfTickets = 0;
+    ticketTypeRequests.map((req) => {
+      numberOfTickets += req.getNoOfTickets();
+      console.log("Total number of tickets:" + numberOfTickets)
+    });
+
+    return numberOfTickets;
+  }
+
+  #groupTicketRequestByType(ticketTypeRequests) {
+    let adultTicketCount = 0;
+    let childTicketCount = 0;
+    let infantTicketCount = 0;
+
+    ticketTypeRequests.map((req) => {
+      const ticketCount = req.getNoOfTickets();
+      switch(req.getTicketType()) {
+        case "ADULT":
+          adultTicketCount += ticketCount;
+          break;
+        case "CHILD":
+          childTicketCount += ticketCount;
+          break;
+        case "INFANT":
+          infantTicketCount += ticketCount;
+          break;
+      }
+    });
+
+    return {
+      "ADULT": adultTicketCount,
+      "CHILD": childTicketCount,
+      "INFANT": infantTicketCount
+    };
   }
 }
--- test/TicketService.test.js
+++ test/TicketService.test.js
@@ -1,18 +1,26 @@
 import assert from 'node:assert/strict';
-import { describe, it, before, mock } from 'node:test';
+import { describe, it, before, mock, beforeEach } from 'node:test';
 
 import TicketTypeRequest from '../src/pairtest/lib/TicketTypeRequest.js';
 
-import TicketPaymentService from '../src/thirdparty/paymentgateway/TicketPaymentService.js';
-import SeatReservationService from '../src/thirdparty/seatbooking/SeatReservationService.js';
-
 describe("TicketService", async () => {
-    const paymentServiceMock = mock.fn();
-    const seatBookingMock = mock.fn();
+    let paymentsMade = false;
+    const paymentServiceMock = mock.fn(class {
+        makePayment(accountId, totalAmountToPay) {
+            paymentsMade = true;
+        }
+    });
+
+    let seatsReserved = false;
+    const seatBookingMock = mock.fn(class {
+        reserveSeat(accountId, totalSeatsToAllocate) {
+            paymentsMade = true;
+        }
+    });
     let ticketService;
 
     before(async() => {
-        //Mpck the external services to prevent them being called, and to track them being invoked
+        //Mock the external services to prevent them being called, and to track them being invoked
         const paymentServiceNamedExports = await import("../src/thirdparty/paymentgateway/TicketPaymentService.js")
         .then(({ default: _, ...rest}) => rest);
 
@@ -29,7 +37,13 @@ describe("TicketService", async () => {
             namedExports: seatReservationServiceNamedExports
         });
 
-        ({ticketService} = await import ("../src/pairtest/TicketService.js"));
+        const TicketServiceClass = (await import ("../src/pairtest/TicketService.js")).default;
+        ticketService = new TicketServiceClass();
+    });
+
+    beforeEach(() => {
+        paymentsMade = false;
+        seatsReserved = false;
     });
 
     describe("Account IDs", () => {
@@ -40,7 +54,57 @@ describe("TicketService", async () => {
         it("should NOT accept non-integer account ids", () => {
             assert.throws(() => ticketService.purchaseTickets(3.141592, []));
         });
+
     });
 
+    describe("should NOT accept more than 25 tickets", () => {
+        it("26 in a single request", () => {
+            assert.throws(() => ticketService.purchaseTickets(1, new TicketTypeRequest("ADULT", 26)));
+        });
 
-})
+        it("26 in a 2 requests", () => {
+            assert.throws(() => ticketService.purchaseTickets(1, 
+                new TicketTypeRequest("ADULT", 13),
+                new TicketTypeRequest("ADULT", 13))
+            );
+        });
+    });
+
+    describe("adult supervision", () => {
+        it("should NOT allow CHILD tickets without ADULT tickets", () => {
+            assert.throws(() => ticketService.purchaseTickets(1, new TicketTypeRequest("CHILD", 1)));
+        });
+
+        it("should NOT allow INFANT tickets without ADULT tickets", () => {
+            assert.throws(() => ticketService.purchaseTickets(1, new TicketTypeRequest("INFANT", 1)));
+        });
+
+        it("should NOT allow more INFANT tickets than ADULT tickets", () => {
+            assert.throws(() => ticketService.purchaseTickets(1,
+                new TicketTypeRequest("ADULT", 1),
+                new TicketTypeRequest("INFANT", 2))
+            );
+        });
+
+        it("should allow equal INFANT tickets and ADULT tickets", () => {
+            assert.doesNotThrow(() => ticketService.purchaseTickets(1,
+                new TicketTypeRequest("ADULT", 1),
+                new TicketTypeRequest("INFANT", 1))
+            );
+        });
+
+        it("should allow more ADULT tickets than INFANT tickets", () => {
+            assert.doesNotThrow(() => ticketService.purchaseTickets(1, 
+                new TicketTypeRequest("ADULT", 2),
+                new TicketTypeRequest("INFANT", 1))
+            );
+        });
+
+        it("should allow CHILD tickets when at least one ADULT ticket", () => {
+            assert.doesNotThrow(() => ticketService.purchaseTickets(1,
+                new TicketTypeRequest("ADULT", 1),
+                new TicketTypeRequest("CHILD", 10))
+            );
+        });
+    });
+});
